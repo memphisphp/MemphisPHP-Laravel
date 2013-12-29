49dd61cc60153dd6cd5c7e419cdb5632513ce45f
Svpernova09 <halo09@gmail.com>
2013-12-29 15:03:19 -0600

    Sponsor Seeding

--- app/database/seeds/SponsorsTableSeeder.php
+++ app/database/seeds/SponsorsTableSeeder.php
@@ -5,14 +5,26 @@ class SponsorsTableSeeder extends Seeder {
 	public function run()
 	{
 		// Uncomment the below to wipe the table clean before populating
-		// DB::table('sponsors')->truncate();
+		DB::table('sponsors')->truncate();
 
 		$sponsors = array(
-
+            'name' => 'Vaco',
+            'link' => 'http://www.vaco.com/memphis/team/diane-tabulog',
+            'imgLink' => 'vaco.jpeg',
+            'active' => '1',
+            'description' => 'Memphis IT Recruiter',
 		);
+        DB::table('sponsors')->insert($sponsors);
 
-		// Uncomment the below to run the seeder
-		// DB::table('sponsors')->insert($sponsors);
+        $sponsors = array(
+            'name' => 'Modern Services Inc',
+            'link' => 'http://www.modernservicesinc.com/',
+            'imgLink' => 'modernservicesinc.jpeg',
+            'active' => '1',
+            'description' => 'Memphis Area IT Firm. Specializing in Application Development, Cabling Infrastructure,
+                                Networking, and Server Support',
+        );
+        DB::table('sponsors')->insert($sponsors);
 	}
 
 }
