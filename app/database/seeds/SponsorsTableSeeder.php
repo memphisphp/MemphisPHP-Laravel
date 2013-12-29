755479e9983ed626b28443c5f8e9572339691256
Svpernova09 <halo09@gmail.com>
2013-12-29 14:53:29 -0600

    Sponsor generated files

---
+++ app/database/seeds/SponsorsTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+class SponsorsTableSeeder extends Seeder {
+
+	public function run()
+	{
+		// Uncomment the below to wipe the table clean before populating
+		// DB::table('sponsors')->truncate();
+
+		$sponsors = array(
+
+		);
+
+		// Uncomment the below to run the seeder
+		// DB::table('sponsors')->insert($sponsors);
+	}
+
+}
